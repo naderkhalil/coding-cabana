b1fb104dc15e4098132f07e93c577401ab890d2a
naderkhalil <nader.khalil@gmail.com>
2021-02-05 01:47:09 -0800

    contact us works

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,242 +5,265 @@ import cayucos from '../images/Sunset.png'
 import helena from '../images/helenamerk.jpeg'
 import Grid from '@material-ui/core/Grid'
 
-class Main extends React.Component {
-  render() {
-    let close = (
-      <div
-        className="close"
-        onClick={() => {
-          this.props.onCloseArticle()
-        }}
-      ></div>
-    )
 
-    return (
-      <div
-        ref={this.props.setWrapperRef}
-        id="main"
-        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+function Main(props) {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [msg, setMsg] = React.useState('')
+  const [open, setOpen] = React.useState(false);
+
+  let close = (
+    <div
+      className="close"
+      onClick={() => {
+        props.onCloseArticle()
+      }}
+    ></div>
+  )
+
+  const submitData = async () => {
+    const response = await fetch("https://4wzjxgkv.brev.dev/api/websiteForm", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        message: msg
+      })
+    });
+    console.log(response)
+    if (response.status===200) {
+      setOpen(true);
+    }
+  }
+
+  return (
+    <div
+      ref={props.setWrapperRef}
+      id="main"
+      style={props.timeout ? { display: 'flex' } : { display: 'none' }}
+    >
+      <article
+        id="companies"
+        className={`${props.article === 'companies' ? 'active' : ''} ${
+          props.articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
       >
-        <article
-          id="companies"
-          className={`${this.props.article === 'companies' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Companies</h2>
-          <Grid container direction="row" justify="center" alignItems="center">
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              style={{ padding: '50px' }}
-              onClick={() => {
-                window.open('https://brev.dev', '_blank')
-              }}
-            >
-              🥞 Brev
-              <p>The shortest stack you'll ever need.</p>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              style={{ padding: '50px' }}
-              onClick={() => {
-                window.open('https://joinglimpse.com', '_blank')
-              }}
-            >
-              🚀 Glimpse
-              <p>Video calls for humans who like humans.</p>
-            </Grid>
-            <Grid item xs={12} sm={6} style={{ padding: '50px' }}>
-              👣 Stealth @ Growth
-              <p>__________________</p>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              style={{ padding: '50px' }}
-              onClick={() => {
-                window.open('https://trove.so', '_blank')
-              }}
-            >
-              🤫 Trove
-              <p>The internet's internet.</p>
-            </Grid>
-          </Grid>
-          {close}
-        </article>
-
-        <article
-          id="people"
-          className={`${this.props.article === 'people' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">People</h2>
+        <h2 className="major">Companies</h2>
+        <Grid container direction="row" justify="center" alignItems="center">
           <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            style={{ textAlign: 'center' }}
+            item
+            xs={12}
+            sm={6}
+            style={{ padding: '50px' }}
+            onClick={() => {
+              window.open('https://brev.dev', '_blank')
+            }}
           >
-            <Grid item xs={4}>
-              <Profile
-                img={nader}
-                name={'Nader Khalil'}
-                twitter={'https://twitter.com/naderlikeladder'}
-                github={'https://github.com/naderlikeladder'}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Profile
-                img={helena}
-                name={'Helena Merk'}
-                twitter={'https://twitter.com/helenamerk'}
-                github={'https://github.com/helenamerk'}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Profile
-                img={nader}
-                name={'Nader Khalil'}
-                twitter={'https://twitter.com/naderlikeladder'}
-                github={'https://github.com/naderlikeladder'}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Profile
-                img={helena}
-                name={'Helena Merk'}
-                twitter={'https://twitter.com/helenamerk'}
-                github={'https://github.com/helenamerk'}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Profile
-                img={nader}
-                name={'Nader Khalil'}
-                twitter={'https://twitter.com/naderlikeladder'}
-                github={'https://github.com/naderlikeladder'}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <Profile
-                img={helena}
-                name={'Helena Merk'}
-                twitter={'https://twitter.com/helenamerk'}
-                github={'https://github.com/helenamerk'}
-              />
-            </Grid>
+            🥞 Brev
+            <p>The shortest stack you'll ever need.</p>
           </Grid>
-          {/* <span className="image main">
-            <img src={pic02} alt="" />
-          </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p> */}
-          {close}
-        </article>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            style={{ padding: '50px' }}
+            onClick={() => {
+              window.open('https://joinglimpse.com', '_blank')
+            }}
+          >
+            🚀 Glimpse
+            <p>Video calls for humans who like humans.</p>
+          </Grid>
+          <Grid item xs={12} sm={6} style={{ padding: '50px' }}>
+            👣 Stealth @ Growth
+            <p>__________________</p>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            style={{ padding: '50px' }}
+            onClick={() => {
+              window.open('https://trove.so', '_blank')
+            }}
+          >
+            🤫 Trove
+            <p>The internet's internet.</p>
+          </Grid>
+        </Grid>
+        {close}
+      </article>
 
-        <article
-          id="location"
-          className={`${this.props.article === 'location' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
+      <article
+        id="people"
+        className={`${props.article === 'people' ? 'active' : ''} ${
+          props.articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
+        <h2 className="major">People</h2>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          style={{ textAlign: 'center' }}
         >
-          <h2 className="major">Location</h2>
-          <span className="image main">
-            <img src={cayucos} alt="" />
-          </span>
-          <p>
-            We're in a charming surf town called Cayucos on California's central
-            coast.
-          </p>
-          {close}
-        </article>
+          <Grid item xs={4}>
+            <Profile
+              img={nader}
+              name={'Nader Khalil'}
+              twitter={'https://twitter.com/naderlikeladder'}
+              github={'https://github.com/naderlikeladder'}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Profile
+              img={helena}
+              name={'Helena Merk'}
+              twitter={'https://twitter.com/helenamerk'}
+              github={'https://github.com/helenamerk'}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Profile
+              img={nader}
+              name={'Nader Khalil'}
+              twitter={'https://twitter.com/naderlikeladder'}
+              github={'https://github.com/naderlikeladder'}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Profile
+              img={helena}
+              name={'Helena Merk'}
+              twitter={'https://twitter.com/helenamerk'}
+              github={'https://github.com/helenamerk'}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Profile
+              img={nader}
+              name={'Nader Khalil'}
+              twitter={'https://twitter.com/naderlikeladder'}
+              github={'https://github.com/naderlikeladder'}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Profile
+              img={helena}
+              name={'Helena Merk'}
+              twitter={'https://twitter.com/helenamerk'}
+              github={'https://github.com/helenamerk'}
+            />
+          </Grid>
+        </Grid>
+        {close}
+      </article>
 
-        <article
-          id="contact"
-          className={`${this.props.article === 'contact' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li>
-                <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
-              </li>
-            </ul>
-          </form>
-          <ul className="icons">
+      <article
+        id="location"
+        className={`${props.article === 'location' ? 'active' : ''} ${
+          props.articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
+        <h2 className="major">Location</h2>
+        <span className="image main">
+          <img src={cayucos} alt="" />
+        </span>
+        <p>
+          We're in a charming surf town called Cayucos on California's central
+          coast.
+        </p>
+        {close}
+      </article>
+
+      <article
+        id="contact"
+        className={`${props.article === 'contact' ? 'active' : ''} ${
+          props.articleTimeout ? 'timeout' : ''
+        }`}
+        style={{ display: 'none' }}
+      >
+        {open && 
+        <h3 style={{color: "green"}}>Your message was sent!</h3>
+        }
+
+        <h2 className="major">Contact</h2>
+        <form onsubmit="return capture();">
+          <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+          <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              value={msg}
+              onChange={e => setMsg(e.target.value)}
+            ></textarea>
+          </div>
+        </form>
+          <ul className="actions">
+            <li>
+              <button onClick={submitData}>Send Message</button>
+            </li>
+            <li>
+              <input type="reset" value="Reset" />
+            </li>
+          </ul>
+        {/* <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://codingcabana.com"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://codingcabana.com" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://codingcabana.com" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://codingcabana.com"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
               </a>
             </li>
-          </ul>
-          {close}
-        </article>
-      </div>
-    )
-  }
+          </ul> */}
+        {close}
+      </article>
+    </div>
+  )
 }
 
 Main.propTypes = {
